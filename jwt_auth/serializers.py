af027a9ecbdf5e6b3c4b9cd4fa836dce357d8827
Vikram Bageja <vikram.bageja@gmail.com>
2019-11-12 19:15:20 +0000

    testing groups

--- jwt_auth/serializers.py
+++ jwt_auth/serializers.py
@@ -1,32 +1,38 @@
-#pylint: disable = no-member, arguments-differ
 from rest_framework import serializers
+from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
-import django.contrib.auth.password_validation as validation
+import django.contrib.auth.password_validation as validations
 from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-
     password = serializers.CharField(write_only=True)
     password_confirmation = serializers.CharField(write_only=True)
 
     def validate(self, data):
-
         password = data.pop('password')
         password_confirmation = data.pop('password_confirmation')
 
         if password != password_confirmation:
             raise ValidationError({'password_confirmation': 'does not match'})
 
-        try:
-            validation.validate_password(password=password)
-        except ValidationError as err:
-            raise serializers.ValidationError({'password': err.messages})
+        # try:
+        #     validations.validate_password(password=password)
+        # except ValidationError as err:
+        #     raise serializers.ValidationError({'password': err.messages})
 
         data['password'] = make_password(password)
         return data
 
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'password_confirmation')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name', )
