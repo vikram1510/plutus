6707d35863510a89d50f2e85539b05b24d808c43
Vikram Bageja <vikram.bageja@gmail.com>
2019-11-13 18:12:40 +0000

    pycache to gitignore and authentication for groups

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -2,13 +2,13 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework.views import APIView, View
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django.conf import settings
 import jwt
+
 from .serializers import UserSerializer, GroupSerializer
-from rest_framework import serializers
-import json
 
 User = get_user_model()
 
@@ -41,11 +41,13 @@ class LoginView(APIView):
         return Response({'token': token, 'message': f'Welcome back {user.username}'})
 
 class GroupIndexCreate(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = GroupSerializer
     def get_queryset(self):
         return self.request.user.groups.all()
 
 class GroupShowUpdateDelete(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     serializer_class = GroupSerializer
     
     def retrieve(self, request, *args, **kwargs):
