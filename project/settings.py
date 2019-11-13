a5aabe91b3fdea6d86a67046eb3a777610d1eb60
Chawit.O <chawit.ocr@gmail.com>
2019-11-13 10:48:57 +0000

    comment out SessionAuthentication, fixing 403 error

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'jwt_auth.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication', # mess with jwt auth
     ],
 }
 
