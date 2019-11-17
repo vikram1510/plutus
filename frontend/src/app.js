378aa25725ee90ac1e2093e3f40580890cca21ee
Chawit.O <chawit.ocr@gmail.com>
2019-11-17 00:25:40 +0000

    frontend expenses: rudimentary create form

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -11,6 +11,7 @@ import Login from './components/auth/Login'
 
 import ExpensesIndex from './components/expenses/ExpensesIndex'
 import ExpensesShow from './components/expenses/ExpensesShow'
+import ExpensesNew from './components/expenses/ExpensesNew'
 
 import FriendsIndex from './components/friends/FriendsIndex'
 import FriendShow from './components/friends/FriendShow'
@@ -23,6 +24,7 @@ const App = () => (
         <Route exact path='/' component={Home} />
         <Route path='/register' component={Register} />
         <Route path='/login' component={Login} />
+        <Route path='/expenses/new' component={ExpensesNew} />
         <Route path='/expenses/:id' component={ExpensesShow} />
         <Route path='/expenses' component={ExpensesIndex} />
         <Route path='/friends/:id' component={FriendShow} />
