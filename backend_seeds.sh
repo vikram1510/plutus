6aaad71d3ffed7fef1f98c47e6c4f215998c4737
Vikram Bageja <vikram.bageja@gmail.com>
2019-11-21 11:02:11 +0000

    modified seeds bash script

--- backend_seeds.sh
+++ backend_seeds.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo "Flushing DB...."
+python manage.py flush
 echo "Running jwt_auth seeds..........."
-python manage.py loaddata jwt_auth/seeds.json
+python manage.py loaddata jwt_auth/demo_seeds.json
 echo "Running expenses seeds..........."
-python manage.py loaddata expenses/seeds.json
+python manage.py loaddata expenses/demo_seeds.json
